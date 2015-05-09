65ae977b1c3dc36e37ccf851341dd8236e89e53c
MinWoo <mwbyun@hotmail.com>
2015-05-10 01:54:18 +0900

    add r1a

---
+++ google-codejam/2015/r1a/b1.cpp
@@ -0,0 +1,62 @@
+
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int __lcm(long long a, long long b) {
+    return a * b / __gcd(a, b);
+}
+
+int main() {
+    int TC;
+    cin >> TC;
+    for ( int tc=1 ; tc<=TC ; ++tc ) {
+        int B, N;
+        cin >> B >> N;
+        vector<int> cut_time(B);
+        for ( int i=0 ; i<B ; ++i ) {
+            cin >> cut_time[i];
+        }
+
+        int lcm = cut_time[0];
+        for ( int i=1 ; i<B ; ++i ) {
+            lcm = __lcm(lcm, cut_time[i]);
+        }
+
+        int rep = 0;
+        for ( int i=0 ; i<B ; ++i ) {
+            rep += lcm / cut_time[i];
+        }
+        N %= rep;
+        N += rep;
+
+        vector<int> wait(B);
+        int mini, minv;
+        for ( int i=0; i<N ; ++i ) {
+            mini = 0;
+            minv = wait[0];
+            for ( int j=0 ; j<wait.size() ; ++j ) {
+                if ( minv > wait[j] ) {
+                    mini = j;
+                    minv = wait[j];
+                }
+            }
+            for ( int j=0 ; j<wait.size() ; ++j ) {
+                wait[j] -= minv;
+            }
+            wait[mini] = cut_time[mini];
+        }
+
+        int res = mini + 1;
+        printf("Case #%d: %d\n", tc, res);
+    }
+}
+
